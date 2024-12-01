d4095ee19c063408f8c8c35b1a0983d9b750ff82
David Greitan <alphabaqpla@outlook.com>
2024-12-01 23:18:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ const { log } = require('./logger.js')
 const logger = new log()
 const { Buffer } = require('node:buffer');
 const replace = require('buffer-replace');
-const proxyProtocolV2 = require('proxy-protocol-v2'); // Импортируем библиотеку для работы с Proxy Protocol v2
 
 const server = dgram.createSocket('udp4');
 let ipArray = {};
 
-serverip = "65.21.175.138"
+serverip = "0.0.0.0"
 serverPort = 19132
 
 proxyip = "0.0.0.0"
@@ -20,29 +19,11 @@ server.on('error', (err) => {
 });
 
 function changePort(buffer) {
-    return replace(buffer, serverPort, proxyPort);
-}
-
-// Функция для добавления заголовка Proxy Protocol v2
-function addProxyProtocolHeader(buffer, rinfo) {
-    const proxyHeader = proxyProtocolV2.v2_encode({
-        remoteFamily: 'IPv4',
-        remoteAddress: rinfo.address,
-        remotePort: rinfo.port,
-        localAddress: serverip,
-        localPort: serverPort,
-        protocol: 'udp'
-    });
-    console.log(rinfo.address, rinfo.port, serverip, serverPort);
-
-    // Возвращаем новый буфер с добавленным заголовком
-    console.log(Buffer.concat([proxyHeader, buffer]));
-    return Buffer.concat([proxyHeader, buffer]);
+    return replace(buffer, serverPort, proxyPort)
 }
 
 function packetReceive(msg, rinfo, sendPort) {
-    console.log(msg, rinfo, sendPort);
-    let type = msg.toString('hex').substr(0, 2);
+    type = msg.toString('hex').substr(0, 2)
     if (rinfo.address !== serverip) {
         var portTime = new Date();
         if (typeof (ipArray[rinfo.port]) === 'undefined') {
@@ -54,38 +35,34 @@ function packetReceive(msg, rinfo, sendPort) {
             ipArray[rinfo.port].socket.on("message", function (msgg, rinfoo) {
                 packetReceive(msgg, rinfoo, ipArray[rinfo.port]['port']);
             });
-        } else {
+        }
+        else {
             ipArray[rinfo.port]['time'] = portTime.getTime();
         }
     }
-
     if (rinfo.address !== serverip) {
-        logger.info(`0x${type} packet received from client: ${rinfo.address}`);
-        const packetWithProxyHeader = addProxyProtocolHeader(msg, rinfo);
-        ipArray[rinfo.port].socket.send(packetWithProxyHeader, 0, packetWithProxyHeader.length, serverPort, serverip);
-    } else if (rinfo.port === serverPort) {
-        type = msg.toString('hex').substr(0, 2);
-        logger.info(`0x${type} packet received from server: ${rinfo.address}`);
+        logger.info(`0x${type} packet received from client: ${rinfo.address}`)
+        ipArray[rinfo.port].socket.send(msg, 0, msg.length, serverPort,
+            serverip);
+    }
+
+    else if (rinfo.port == serverPort) {
+        type = msg.toString('hex').substr(0, 2)
+        logger.info(`0x${type} packet received from server: ${rinfo.address}`)
 
         var currentTime = new Date().getTime();
         if ((currentTime - ipArray[sendPort]['time']) > 30000) {
             ipArray[sendPort].socket.close();
             delete ipArray[sendPort];
-        } else {
-            // Обработаем входящий пакет с Proxy Protocol v2, если он был
-            const decoded = proxyProtocolV2.decode(msg);
-            if (decoded !== null) {
-                logger.info('Proxy Protocol v2 header decoded:', decoded);
-            }
-
-            const packetWithProxyHeader = addProxyProtocolHeader(msg, rinfo);
-            server.send(packetWithProxyHeader, 0, packetWithProxyHeader.length, ipArray[sendPort]['port'], ipArray[sendPort]['ip']);
+        }
+        else {
+            server.send(changePort(msg), 0, msg.length, ipArray[sendPort]['port'], ipArray[sendPort]['ip']);
         }
     }
 }
 
 server.on('message', (msg, info) => {
-    packetReceive(msg, info, info.port);
+    packetReceive(msg, info, info.port)
 });
 
 server.on('listening', () => {
@@ -93,4 +70,5 @@ server.on('listening', () => {
     logger.info(`server listening ${address.address}:${address.port}`);
 });
 
-server.bind({ address: '0.0.0.0', port: 19133 });
+
+server.bind({ address: '0.0.0.0', port: 19149 });
