797499e481a105372771a8a22eb327e0eb13a2e5
David Greitan <alphabaqpla@outlook.com>
2024-12-02 01:25:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ function packetReceive(msg, rinfo, sendPort) {
             delete ipArray[sendPort];
         }
         else {
-            server.send(msg, 0, msg.length, ipArray[sendPort]['port'], ipArray[sendPort]['ip']);
+            server.send(replace(msg, '19133', '19132'), 0, replace(msg, '19133', '19132').length, ipArray[sendPort]['port'], ipArray[sendPort]['ip']);
         }
     }
 }
